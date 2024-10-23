b8b4d787ebb7b52e4713772a49b9d2298ec8a3d7
CorujasBR <166850474+CorujasBR@users.noreply.github.com>
2024-10-23 19:09:32 -0300

    Add files via upload

---
+++ modulo2/aula2_questao2.py
@@ -0,0 +1,12 @@
+Result = "O resultado é"
+
+x = 10
+y = 3.5
+
+z =(x + y)
+print(Result, x, y, z)
+print(type(Result))
+print(type(x))
+print(type(y))
+print(type(z))
+print()
---
+++ modulo2/aula2_questao4.py
@@ -0,0 +1,7 @@
+
+saldo = 500.0
+rendimento1 = saldo * 1.01
+rendimento1 = rendimento1 * 1.01
+rendimento1 = rendimento1 * 1.01
+print("Após 3 meses meu novo saldo é")
+print(rendimento1)
